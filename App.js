8c63cd25c3f4bdd35ada4c93075fbd2812f28a6b
Samuel Kelechi <49629174+SamuelKelechi@users.noreply.github.com>
2020-12-30 13:14:47 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import MainFile from './Component/MainFile'
+
+
+function App() {
+  return (
+    <div>
+    <MainFile />
+    </div>
+  );
+}
+
+export default App;
---
+++ Practice.js
@@ -0,0 +1,12 @@
+import './Practice.css'
+function Practice() {
+    return (
+      <div className='main'>hello
+      
+      
+      
+      </div>
+    );
+  }
+  
+  export default Practice;
